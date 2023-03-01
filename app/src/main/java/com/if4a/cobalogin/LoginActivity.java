8e6562b204e4bd5b83b30e8bfcc222c3ba0b8287
Wesley Maximilliano <wesleymaxi56@mhs.mdp.ac.id>
2023-03-01 08:49:00 +0700

    Update

--- app/src/main/java/com/if4a/cobalogin/LoginActivity.java
+++ app/src/main/java/com/if4a/cobalogin/LoginActivity.java
@@ -10,15 +10,44 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText etUsername, etPassword;
+    private Button btnLogin;
+    private String username,password;
+    KendaliLogin KL = new KendaliLogin();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+        btnLogin = findViewById(R.id.btn_login);
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username = etUsername.getText().toString();
+                password = etPassword.getText().toString();
 
+                if(username.trim().equals("")){
+                    etUsername.setError("Username Tidak Boleh Kosong!");
+                }
+                else if (password.trim().equals("")){
+                    etPassword.setError("Password Tidak Boleh Kosong!");
+                }
+                else {
+                    if(username.equals("wesley") && password.equals("apaya?")){
+                        KL.setPref(LoginActivity.this,MainActivity.keySPusername,username);
+                        startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                        finish();
+                    }
+                    else {
+                        Toast.makeText(LoginActivity.this, "Username atau Password Belum Tepat",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
 
         }
     }
